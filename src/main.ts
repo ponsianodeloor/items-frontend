ac4cc55fc1e2883beae73d41f6165d657ba97863
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-03-24 17:34:48 -0500

    configuracion de axios para consumo de api por una variable configurada en las de entorno

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import axios from "axios";
+import { environment } from './environments/environment';
 
+axios.defaults.baseURL = environment.apiUrl;
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
