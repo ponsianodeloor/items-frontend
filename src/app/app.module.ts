9e320fe2da9771d7de87680f933f8a228e6683a6
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-03-24 17:42:51 -0500

    creacion de componntes y servicios para consumo de api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OverviewComponent } from './components/item/overview/overview.component';
+import { CreateComponent } from './components/item/create/create.component';
+import { DetailComponent } from './components/item/detail/detail.component';
+import { EditComponent } from './components/item/edit/edit.component';
+import { DeleteComponent } from './components/item/delete/delete.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent,
+    CreateComponent,
+    DetailComponent,
+    EditComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
