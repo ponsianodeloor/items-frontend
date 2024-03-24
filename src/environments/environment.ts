4268531a9079c4fadc0b1bd30f71ea4b85e9ee9a
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-03-24 17:32:09 -0500

    configuracion de variable de entorno

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080',
+  /*firebase: {
+    apiKey: "AIzaSyD-4v",
+    authDomain: "angularfire-",
+    databaseURL: "https://angularfire-",
+    projectId: "angularfire-",
+    storageBucket: "angularfire-",
+    messagingSenderId: "1",
+    appId: "1",
+    measurementId: "G-"
+  }*/
+};
