da1ab912748fc14661b9fdb4d8b79fb90b19e26f
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-03-25 00:35:08 -0500

    servicio para consumir datos

--- src/app/services/item/item.service.ts
+++ src/app/services/item/item.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Item } from '../../interfaces/item';
+import axios from 'axios';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,40 @@ import { Injectable } from '@angular/core';
 export class ItemService {
 
   constructor() { }
+
+  getItems(): Promise<Item[]> {
+    return axios.get('/api/v1/items/all')
+      .then(response => response.data);
+  }
+
+  getItem(id: number): Promise<Item> {
+    return axios.get(`/api/v1/items/${id}`)
+      .then(response => response.data);
+  }
+
+  /**
+  createItem(item: Item): Promise<Item> {
+    return axios.post('/api/v1/items', item)
+      .then(response => response.data);
+  }
+  */
+  createItem(request:any): Promise<Item> {
+    let item = {
+      name: request.title,
+      description: request.description
+    }
+    return axios.post('/api/v1/items', item)
+      .then(response => response.data);
+  }
+
+  updateItem(item: Item): Promise<Item> {
+    return axios.put(`/api/v1/items/${item.id}`, item)
+      .then(response => response.data);
+  }
+
+  deleteItem(id: number): Promise<Item> {
+    return axios.delete(`/api/v1/items/${id}`)
+      .then(response => response.data);
+  }
+
 }
